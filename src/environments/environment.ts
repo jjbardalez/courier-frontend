33a13b05ea7d5517a9e3b004a615ec7da16a00b5
jcaychob <jjbardalez@stefanini.com>
2023-04-24 20:02:42 -0500

    ajustes a módulos
    ajustes a todos los modulos para ajustarlos según la dimensión  del prototipo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,4 @@ export const environment = {
 
   production: true,
 
-  // Backend Utils
-  API_URL_UTILS: 'https://nsf-backend-util.azurewebsites.net/api/',
-
-  // Backend Billing
-  API_URL_BILLING: 'https://nsf-backend-billing.azurewebsites.net/api/',
-
 };
